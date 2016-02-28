81e26f70b883f9987414b4650de00ff327c7fca3
Lilisys <jesswu@umich.edu>
2016-02-28 10:17:45 -0500

    added the end scene

---
+++ Assets/Scripts/GameEnd.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using Leap;
+
+public class GameEnd : MonoBehaviour
+{
+      
+    public bool ________________________________;
+
+    Leap.Controller controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = new Leap.Controller();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Leap.Frame frame = controller.Frame();
+        Leap.Hand hand = frame.Hands.Frontmost;
+        Vector normalizedHandPos = Leap.Vector.Zero;
+
+        if (frame.InteractionBox.IsValid)
+        {
+            normalizedHandPos = frame.InteractionBox.NormalizePoint(hand.PalmPosition, true);
+        }
+        Vector3 handPos = normalizedHandPos.ToUnityScaled();
+        //means they were all the way left
+        if (handPos.x == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+    }
+}
