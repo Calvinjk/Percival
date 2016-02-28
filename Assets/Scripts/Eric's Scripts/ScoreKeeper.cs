8295fd4f5900bccffa63e06b31e18540b856229a
wueric2 <wueric2@msu.edu>
2016-02-28 10:40:14 -0500

    ScoreKeeper created

---
+++ Assets/Scripts/Eric's Scripts/ScoreKeeper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public int score = 0;
+    public int scoreMultiplier = 2721;
+    Text playerText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score = (int)(Time.time * scoreMultiplier);
+        //playerText.text = "Score:" + score.ToString();
+        //gameObject.text = "hello";
+        GetComponent<Text>().text = "Score:" + score.ToString();
+	}
+}
