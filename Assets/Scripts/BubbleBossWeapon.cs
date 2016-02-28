e58021d5a1532ad33a02035de7130200827d49bd
Calvin Kopitz <calvinjk@umiGotmilk21ch.edu>
2016-02-28 04:43:17 -0500

    BigBootyBoss

---
+++ Assets/Scripts/BubbleBossWeapon.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleBossWeapon : MonoBehaviour {
+
+    public GameObject   littleMePrefab;
+    public float        maxWeaponCooldown   = 1f;
+    public float        bulletSize          = .20f;
+    public float        bulletSpeed         = 100f;
+
+    public bool ______________________________;
+
+    public GameObject littleMeInstance;
+    public float curWeaponCooldown = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (curWeaponCooldown <= 0) {
+            curWeaponCooldown = maxWeaponCooldown;
+            littleMeInstance = Shoot();
+            littleMeInstance.GetComponent<Rigidbody>().AddForce(new Vector3( Random.Range(-1f, 1f), Random.Range(-1f, 2f), -1f) * bulletSpeed * Random.Range(1,2));
+        } else {
+            curWeaponCooldown -= Time.deltaTime;
+        }
+	}
+
+    GameObject Shoot() {
+        return Instantiate(littleMePrefab, transform.position, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360))) as GameObject;
+    }
+}
---
+++ Assets/Scripts/KitingEnemyScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KitingEnemyScript : MonoBehaviour {
+
+    public float initialSpawnDist   = 400f;
+    public float approachSpeed      = 1f;
+    public float killDistance       = 10f;
+
+    public bool _____________________________;
+
+    public GameObject   cam;
+    public GameObject   player;
+
+	// Use this for initialization
+	void Start () {
+        cam = GameObject.Find("Main Camera");
+        Vector3 camPos = cam.transform.position;
+        transform.position = new Vector3(camPos.x, camPos.y + 4f, camPos.z + initialSpawnDist);
+
+        player = GameObject.Find("Sammy the Smog Cloud");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float camSpeed = cam.GetComponent<Rigidbody>().velocity.z;
+        GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, camSpeed - approachSpeed);
+        //Debug.Log(Mathf.Abs(transform.position.z - player.transform.position.z));
+        if (Mathf.Abs(transform.position.z - player.transform.position.z) < killDistance) {
+            Destroy(gameObject);
+        }
+    }
+}
