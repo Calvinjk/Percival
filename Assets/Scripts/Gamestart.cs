655405b87bc53504b44fcaf37f64a97663a914ce
Calvin Kopitz <calvinjk@umiGotmilk21ch.edu>
2016-02-29 21:29:35 -0500

    Control scheme works up through the title screen.  Text to start changes depending on controls selected.  Actual controls do not work yet

--- Assets/Scripts/Gamestart.cs
+++ Assets/Scripts/Gamestart.cs
@@ -1,36 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Gamestart : MonoBehaviour {
 
 
     public bool ________________________________;
-
+    public Globals globals;
+    public Text startText;
     Leap.Controller controller;
 
     // Use this for initialization
-    void Start ()
-    {
-        controller = new Leap.Controller();
+    void Start () {
+        globals = (Globals)GameObject.Find("Globals").GetComponent(typeof(Globals));
+        startText = GameObject.Find("HowToStart").GetComponent<Text>();
 
+        //Only do leapMotion stuff if leapMotion is the controlScheme.  Hopefully will fix crashing problem
+        if (globals.controlScheme == Globals.ControlScheme.LeapMotion) {
+            controller = new Leap.Controller();
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Leap.Frame frame = controller.Frame();
-        Leap.Hand hand = frame.Hands.Frontmost;
+        switch (globals.controlScheme) {
+            case Globals.ControlScheme.LeapMotion:
+                Leap.Frame frame = controller.Frame();
 
-        float bluh = hand.GrabStrength;
+                startText.text = "Make a fist to start!";
 
-        if (bluh == 1) {
-            SceneManager.LoadScene(1);
+                if (frame.Hands.Frontmost.GrabStrength == 1) {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                }
+                break;
+            case Globals.ControlScheme.Mouse:
+                startText.text = "Click to start!";
+                if (Input.GetKeyUp(KeyCode.Mouse0)) {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                }   
+                break;
+            case Globals.ControlScheme.Keyboard:
+                startText.text = "Press Enter to start!";
+                if (Input.GetKeyUp(KeyCode.Return)) {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                }
+                break;
+            default:
+                Debug.LogError("Incorrect control scheme selected");
+                break;
         }
-
-    }
-    void OnCollisionEnter(Collision coll)
-    {
-        SceneManager.LoadScene(0);
-        Destroy(coll.gameObject);
     }
 }
